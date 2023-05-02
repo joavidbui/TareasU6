a7c454d6b67b2a97f4c872a435fb9ac5482975ff
joavidbui <joavidbui@alu.edu.gva.es>
2023-05-02 15:32:12 +0200

    tests 4, 5, 6 FizzBuzz

--- Tareas-TDD/src/FizzBuzz.java
+++ Tareas-TDD/src/FizzBuzz.java
@@ -1,6 +1,7 @@
 public class FizzBuzz {
     public static String getFizzBuzz(int input) {
-        if (input % 3 == 0) return  "Fizz";
+        if (input % 3 == 0) return "Fizz";
+        if (input % 5 == 0) return "Buzz";
         return String.valueOf(input);
     }
 }
--- Tareas-TDD/src/FizzBuzzTests.java
+++ Tareas-TDD/src/FizzBuzzTests.java
@@ -4,17 +4,25 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class FizzBuzzTests {
     @ParameterizedTest
-    @ValueSource(ints = {1, 2})
+    @ValueSource(ints = {1, 2, 4})
     void notMutiple3nor5_shouldReturnNumberAsString (int number) {
         String actualOutput = FizzBuzz.getFizzBuzz(number);
         String desiredOutput = String.valueOf(number);
         Assertions.assertEquals(actualOutput, desiredOutput);
     }
     @ParameterizedTest
-    @ValueSource(ints = {3})
+    @ValueSource(ints = {3, 6})
     void multiple3_shouldReturnFizz (int number) {
         String actualOutput = FizzBuzz.getFizzBuzz(number);
         String desiredOutput = "Fizz";
+        Assertions.assertEquals(actualOutput, desiredOutput);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {5})
+    void multiple4_shouldReturnBuzz (int number) {
+        String actualOutput = FizzBuzz.getFizzBuzz(number);
+        String desiredOutput = "Buzz";
         Assertions.assertEquals(actualOutput, desiredOutput);
     }
 }
