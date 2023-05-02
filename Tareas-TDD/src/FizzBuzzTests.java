3d3a3addc80fa5fabfcfe5d63150259a5de184fd
joavidbui <joavidbui@alu.edu.gva.es>
2023-05-02 15:33:13 +0200

    tests 7, 8, 9, 10 FizzBuzz

--- Tareas-TDD/src/FizzBuzzTests.java
+++ Tareas-TDD/src/FizzBuzzTests.java
@@ -4,14 +4,14 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class FizzBuzzTests {
     @ParameterizedTest
-    @ValueSource(ints = {1, 2, 4})
+    @ValueSource(ints = {1, 2, 4, 7, 8})
     void notMutiple3nor5_shouldReturnNumberAsString (int number) {
         String actualOutput = FizzBuzz.getFizzBuzz(number);
         String desiredOutput = String.valueOf(number);
         Assertions.assertEquals(actualOutput, desiredOutput);
     }
     @ParameterizedTest
-    @ValueSource(ints = {3, 6})
+    @ValueSource(ints = {3, 6, 9})
     void multiple3_shouldReturnFizz (int number) {
         String actualOutput = FizzBuzz.getFizzBuzz(number);
         String desiredOutput = "Fizz";
@@ -19,7 +19,7 @@ public class FizzBuzzTests {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {5})
+    @ValueSource(ints = {5, 10})
     void multiple4_shouldReturnBuzz (int number) {
         String actualOutput = FizzBuzz.getFizzBuzz(number);
         String desiredOutput = "Buzz";
