4c3ca3bd959ad0c2bb92ae294527fdd30205af7c
joavidbui <joavidbui@alu.edu.gva.es>
2023-05-05 10:40:26 +0200

    JTests v0.1

--- DivisiblePor3/src/DivisiblePor3Tests.java
+++ DivisiblePor3/src/DivisiblePor3Tests.java
@@ -1,5 +1,17 @@
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class DivisiblePor3Tests {
+    @ParameterizedTest
+    @ValueSource(ints = {2, 6, 251, -3})
+    void esDivisibleEntreTres_devuelveTrue(int input) {
+        Assertions.assertTrue(DivisiblePor3.esDivisiblePor3(input));
+    }
 
+    @ParameterizedTest
+    @ValueSource(ints = {4, 7, 250, 7})
+    void noEsDivisibleEntreTres_devuelveFalse(int input) {
+        Assertions.assertFalse(DivisiblePor3.esDivisiblePor3(input));
+    }
 }
