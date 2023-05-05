67c7d8cda79a73418b2843cb95a281637765c39b
Kikoromero97 <kikoromero1997@gmail.com>
2023-05-05 10:28:22 +0200

    DivisiblePor3 Clase.

--- DivisiblePor3/src/DivisiblePor3.java
+++ DivisiblePor3/src/DivisiblePor3.java
@@ -1,3 +1,18 @@
-public class DivisiblePor3 {
 
+public class DivisiblePor3 {
+    public static boolean esDivisiblePor3 (int input) {
+        int suma = 0, resultadoDivision = 0;
+        String numeroEntero = "";
+
+        for (int i = 1; i <= input; i++) {
+            numeroEntero = numeroEntero + "" + i;
+            suma += suma + i;
+        }
+        resultadoDivision = Integer.parseInt(numeroEntero) / suma;
+        if (resultadoDivision % 3 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
