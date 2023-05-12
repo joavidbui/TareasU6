480f180f077b7841e009207d059cb984d9d87ec9
joavidbui <joavidbui@alu.edu.gva.es>
2023-05-12 10:23:29 +0200

    Tasca4 - final array

--- RefactorIF1/src/DataType.java
+++ RefactorIF1/src/DataType.java
@@ -1,4 +1,9 @@
+import java.util.Arrays;
+
 public class DataType {
+
+    private static final int[] DATE_CODES = {702, 1082, 1083, 1114, 1184, 1266, 12403};
+    private static final int[] NUMERIC_CODES = {20, 21, 23, 24, 26, 700, 701, 790, 1700, 2202, 2203, 2204, 2205, 2206, 3734, 3769, 12396};
     public static void main(String[] args) {
         System.out.println(DataType.gd(20));
         System.out.println(DataType.gd(702));
@@ -6,10 +11,12 @@ public class DataType {
     }
 
     public static String gd(int code) {
-        return switch (code) {
-            case 702, 1082, 1083, 1114, 1184, 1266, 12403 -> "DATE";
-            case 20, 21, 23, 24, 26, 700, 701, 790, 1700, 2202, 2203, 2204, 2205, 2206, 3734, 3769, 12396 -> "NUMERIC";
-            default -> "STRING";
-        };
+        if (Arrays.stream(DATE_CODES).anyMatch(i -> i == code)) {
+            return "DATE";
+        }
+        if (Arrays.stream(NUMERIC_CODES).anyMatch(i -> i == code)) {
+            return "NUMERIC";
+        }
+        return "STRING";
     }
 }
